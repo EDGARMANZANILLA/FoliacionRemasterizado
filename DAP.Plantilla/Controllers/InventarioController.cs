ee36a0f4d39950802f473ced01663108e317fdf9
EDGARMANZANILLA <al045356@uacam.mx>
2021-04-18 23:42:13 -0500

    Actualizacion de pantallas principales del inventario

---
+++ DAP.Plantilla/Controllers/InventarioController.cs
@@ -0,0 +1,93 @@
+﻿using DAP.Plantilla.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DAP.Plantilla.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        public ActionResult Index()
+        {
+
+            List<InventarioModel> BancosMostrar = new List<InventarioModel>();
+
+            // var InventariosActivos = Negocios.FoliacionNegocios.ObtenerInventarioActivo();
+
+            // foreach (var inventarioBanco in InventariosActivos)
+            // {
+            //InventarioModel NuevoBanco = new InventarioModel();
+            //NuevoBanco.Id = inventarioBanco.Id;
+            //NuevoBanco.NombreBanco = inventarioBanco.Tbl_CuentasBancarias.NombreBanco;
+            //NuevoBanco.FormasDisponibles = inventarioBanco.FormasDisponibles;
+            //NuevoBanco.UltimoFolioInventario = inventarioBanco.UltimoFolioInventario;
+            //NuevoBanco.UltimoFolioQuincena = inventarioBanco.UltimoFolioQuincena;
+            //NuevoBanco.FormasQuincena1 = inventarioBanco.FormasQuincena1;
+            //NuevoBanco.FormasQuincena2 = inventarioBanco.FormasQuincena2;
+            //NuevoBanco.EstimadoMeses = inventarioBanco.EstimadoMeses;
+
+            //BancosMostrar.Add(NuevoBanco);
+
+            // }
+
+            InventarioModel NuevoBanco = new InventarioModel();
+            NuevoBanco.Id = 1;
+            NuevoBanco.NombreBanco = "banco";
+            NuevoBanco.UltimoFolioInventario = 150;
+            NuevoBanco.UltimoFolioQuincena = 10;
+            NuevoBanco.FormasQuincena1 = 12;
+            NuevoBanco.FormasQuincena2 = 11;
+            NuevoBanco.EstimadoMeses = 5;
+
+            BancosMostrar.Add(NuevoBanco);
+
+
+
+
+            return View(BancosMostrar);
+        }
+
+
+        public ActionResult Ajustar()
+        {
+            return View();
+        }
+
+
+        public ActionResult Solicitar()
+        {
+            return View();
+        }
+
+        public ActionResult nuevoParcial() 
+        {
+
+            List<InventarioModel> BancosMostrar = new List<InventarioModel>();
+
+
+
+            InventarioModel NuevoBanco = new InventarioModel();
+            NuevoBanco.Id = 1;
+            NuevoBanco.NombreBanco = "banco";
+            NuevoBanco.UltimoFolioInventario = 150;
+            NuevoBanco.UltimoFolioQuincena = 10;
+            NuevoBanco.FormasQuincena1 = 12;
+            NuevoBanco.FormasQuincena2 = 11;
+            NuevoBanco.EstimadoMeses = 5;
+
+            BancosMostrar.Add(NuevoBanco);
+
+
+
+
+            return PartialView(BancosMostrar);
+           
+        }
+
+
+    }
+}
---
+++ DAP.Plantilla/Models/InventarioModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAP.Plantilla.Models
+{
+    public class InventarioModel
+    {
+
+        public int Id { get; set; }
+
+        public string NombreBanco { get; set; }
+
+        public int FormasDisponibles { get; set; }
+
+        public int UltimoFolioInventario { get; set; }
+
+        public int UltimoFolioQuincena { get; set; }
+
+        public Nullable<int> FormasQuincena1 { get; set; }
+
+        public Nullable<int> FormasQuincena2 { get; set; }
+
+        public Nullable<decimal> EstimadoMeses { get; set; }
+
+
+
+    }
+}
